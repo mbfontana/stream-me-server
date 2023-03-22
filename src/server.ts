e8245cc0d058f7c204b029fa35352ae21905b714
Matheus Fontana <mbfontana@outlook.com>
2023-03-22 18:01:03 -0300

    Adding CORS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { adminJs, adminJsRouter } from "./admin";
 import { database } from "./database";
 import { router } from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
 app.use(adminJs.options.rootPath, adminJsRouter);
